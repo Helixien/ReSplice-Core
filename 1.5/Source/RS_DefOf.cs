5a0fdc2ebea0fbc1b4de635abcf52d613413eae0
Taranchuk <nfhfyxer@gmail.com>
2024-04-27 22:45:21 +0300

    c# code added

---
+++ 1.5/Source/RS_DefOf.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ReSpliceCore
+{
+    [DefOf]
+    public static class RS_DefOf
+    {
+        public static SoundDef RS_GeneCentrifuge_Ambience, RS_XenoGermDuplicator_Ambience;
+        public static JobDef RS_InsertThingIntoProcessor;
+        public static ThingDef RS_GeneCentrifuge, RS_XenoGermDuplicator;
+    }
+}
---
+++ 1.5/Source/RS_Utils.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace ReSpliceCore
+{
+    [StaticConstructorOnStartup]
+    public static class RS_Utils
+    {
+        public static List<ThingDef> allGenepacks = new List<ThingDef>();
+        static RS_Utils()
+        {
+            foreach (var def in DefDatabase<ThingDef>.AllDefs)
+            {
+                if (def.thingClass != null && typeof(Genepack).IsAssignableFrom(def.thingClass))
+                {
+                    allGenepacks.Add(def);
+                }
+            }
+        }
+
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
+}
