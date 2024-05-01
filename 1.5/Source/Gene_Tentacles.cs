41e8249dba353cbc81611c33197a1146bd8b88e3
Taranchuk <nfhfyxer@gmail.com>
2024-05-01 10:04:48 +0300

    gene tentacles changes

---
+++ 1.5/Source/Gene_Tentacles.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse;
+
+namespace ReSpliceCore
+{
+    public abstract class Gene_Tentacles : Gene
+    {
+        public abstract HediffDef TentacleDef { get; }
+        public override void PostAdd()
+        {
+            base.PostAdd();
+            if (pawn.health.hediffSet.GetFirstHediffOfDef(TentacleDef) is null)
+            {
+                var countToAdd = Rand.RangeInclusive(1, 2);
+                var bodyParts = pawn.RaceProps.body.GetPartsWithDef(TentacleDef.defaultInstallPart).InRandomOrder().ToList();
+                if (bodyParts.Count >= countToAdd)
+                {
+                    for (var i = 0; i < countToAdd; i++)
+                    {
+                        var part = bodyParts[i];
+                        if (pawn.health.hediffSet.PartIsMissing(part) is false
+                            && pawn.health.hediffSet.HasDirectlyAddedPartFor(part) is false)
+                        {
+                            pawn.health.AddHediff(TentacleDef, part);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class Gene_FleshTentacles : Gene_Tentacles
+    {
+        public override HediffDef TentacleDef => HediffDefOf.Tentacle;
+    }
+
+    public class Gene_FleshWhips : Gene_Tentacles
+    {
+        public override HediffDef TentacleDef => HediffDefOf.FleshWhip;
+    }
+}
