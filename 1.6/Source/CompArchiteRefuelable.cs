93bbc488bdbc8eff9b13631b64b779dcafb8defa
SokyranTheDragon <exploder1531@gmail.com>
2025-10-08 22:32:03 +0200

    Xenogerm duplicator improvements
    - Configurable target archite capsule level
      - Custom code to round it to an integer when using the slider
    - Ability to eject archite capsules
      - Custom code so it says "eject archite capsules" rather than "eject fuel"
    - Changed "Target fuel level" text to "Target archite capsule amount"

---
+++ 1.6/Source/CompArchiteRefuelable.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ReSpliceCore;
+
+[StaticConstructorOnStartup]
+public class CompArchiteRefuelable : CompRefuelable
+{
+    public static readonly Texture2D EjectContents = ContentFinder<Texture2D>.Get("UI/Designators/EjectFuel");
+
+    public override IEnumerable<Gizmo> CompGetGizmosExtra()
+    {
+        foreach (var gizmo in base.CompGetGizmosExtra())
+        {
+            if (gizmo is Gizmo_SetFuelLevel)
+                yield return new Gizmo_SetFuelLevel_Rounded(this);
+            else
+                yield return gizmo;
+        }
+
+        // CompRefuelable supports an eject gizmo/designator, but its label is always "eject fuel".
+        // No need for cancel gizmo, as the designator will automatically handle it. In fact, getting rid of it adds extra complexity.
+        if (Fuel > 0 && parent.Map.designationManager.DesignationOn(parent, DesignationDefOf.EjectFuel) == null)
+        {
+            var ejectGizmo = new Command_Action
+            {
+                defaultLabel = "RS.EjectArchites".Translate(),
+                defaultDesc = "RS.EjectArchitesDesc".Translate(),
+                icon = EjectContents,
+                Order = -19.9f, // Designator.CreateReverseDesignationGizmo uses -20, but use slightly less to be ahead of deconstruct gizmo
+                action = () => parent.Map.designationManager.AddDesignation(new Designation(parent, DesignationDefOf.EjectFuel)),
+                activateSound = SoundDefOf.Tick_Tiny,
+            };
+
+            if (explosiveComp is { wickStarted: true })
+                ejectGizmo.Disable("AboutToExplode".Translate());
+
+            yield return ejectGizmo;
+        }
+    }
+
+    public override string CompInspectStringExtra()
+    {
+        if (!Props.targetFuelLevelConfigurable)
+            return base.CompInspectStringExtra();
+
+        // Need to temporarily set the target fuel level configurable to false, so it doesn't add ConfiguredTargetFuelLevel string.
+        // Change "Target fuel level" to "Target archite capsule level".
+        try
+        {
+            Props.targetFuelLevelConfigurable = false;
+
+            var text = base.CompInspectStringExtra();
+            text += "\n" + "RS.TargetArchiteCapsuleLevel".Translate(TargetFuelLevel.ToStringDecimalIfSmall());
+
+            return text;
+        }
+        finally
+        {
+            Props.targetFuelLevelConfigurable = true;
+        }
+    }
+}
---
+++ 1.6/Source/Gizmo_SetFuelLevel_Rounded.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using UnityEngine;
+
+namespace ReSpliceCore;
+
+public class Gizmo_SetFuelLevel_Rounded(CompRefuelable refuelable) : Gizmo_SetFuelLevel(refuelable)
+{
+    public override float Target
+    {
+        get => base.Target;
+        set => refuelable.TargetFuelLevel = Mathf.Clamp(Mathf.RoundToInt(value * refuelable.Props.fuelCapacity), 0, refuelable.Props.fuelCapacity);
+    }
+}
